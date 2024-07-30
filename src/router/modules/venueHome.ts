c85b3ee83f7857a0a2bd3a4961c78a4456373916
suqin <suqin@whitesand.online>
2024-07-30 13:12:26 +0700

    游戏场馆路由

---
+++ src/router/modules/venueHome.ts
@@ -0,0 +1,21 @@
+import Layout from "/@/layout/venueHome/index.vue";
+
+const VenueHome = {
+	component: Layout,
+	children: [
+		{
+			path: "/sports",
+			name: "sports",
+			component: () => import("/@/views/venueHome/sports/sports.vue"),
+			meta: { title: "体育" },
+		},
+		{
+			path: "/lottery",
+			name: "lottery",
+			component: () => import("/@/views/venueHome/lottery/lottery.vue"),
+			meta: { title: "彩票" },
+		},
+	],
+};
+
+export default VenueHome;
