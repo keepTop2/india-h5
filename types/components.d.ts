6b7aeae1305bccc1b807892ce5d8f6bda547391c
suqin <suqin@whitesand.online>
2024-10-14 19:34:25 +0700

    fix

--- types/components.d.ts
+++ types/components.d.ts
@@ -5,55 +5,55 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 import '@vue/runtime-core'
 
-export {};
+export {}
 
-declare module "@vue/runtime-core" {
-	export interface GlobalComponents {
-		AreaCodePicker: typeof import("./../src/components/AreaCodePicker/AreaCodePicker.vue")["default"];
-		Button: typeof import("./../src/components/Button/Button.vue")["default"];
-		Carousel: typeof import("./../src/components/Carousel/Carousel.vue")["default"];
-		Collapse: typeof import("./../src/components/Collapse/index.vue")["default"];
-		CollapseItem: typeof import("./../src/components/Collapse/CollapseItem.vue")["default"];
-		CollapseTransition: typeof import("./../src/components/Collapse/CollapseTransition.vue")["default"];
-		copy: typeof import("./../src/components/SvgIcon/index copy.vue")["default"];
-		DateRangeSelect: typeof import("./../src/components/DateRangeSelect/DateRangeSelect.vue")["default"];
-		FormInput: typeof import("./../src/components/Input/formInput.vue")["default"];
-		Hcaptcha: typeof import("./../src/components/Hcaptcha/hcaptcha.vue")["default"];
-		MultipleSelect: typeof import("./../src/components/MultipleSelect/MultipleSelect.vue")["default"];
-		Nodata: typeof import("./../src/components/Nodata/index.vue")["default"];
-		NoGameImg: typeof import("./../src/components/NoGameImg/NoGameImg.vue")["default"];
-		Notify: typeof import("./../src/components/Notify/Notify.vue")["default"];
-		RedbagRainCountdown: typeof import("./../src/components/redbagRainCountdown/index.vue")["default"];
-		RouterLink: typeof import("vue-router")["RouterLink"];
-		RouterView: typeof import("vue-router")["RouterView"];
-		SingleSelect: typeof import("./../src/components/SingleSelect/SingleSelect.vue")["default"];
-		Spin: typeof import("./../src/components/Spin/Spin.vue")["default"];
-		SvgIcon: typeof import("./../src/components/svgIcon/index.vue")["default"];
-		Tabs: typeof import("./../src/components/Tabs/Tabs.vue")["default"];
-		VanActionSheet: typeof import("vant/es")["ActionSheet"];
-		VanBadge: typeof import("vant/es")["Badge"];
-		VanButton: typeof import("vant/es")["Button"];
-		VanCell: typeof import("vant/es")["Cell"];
-		VanCellGroup: typeof import("vant/es")["CellGroup"];
-		VanDatePicker: typeof import("vant/es")["DatePicker"];
-		VanDropdownItem: typeof import("vant/es")["DropdownItem"];
-		VanDropdownMenu: typeof import("vant/es")["DropdownMenu"];
-		VanField: typeof import("vant/es")["Field"];
-		VanImage: typeof import("vant/es")["Image"];
-		VanIndexAnchor: typeof import("vant/es")["IndexAnchor"];
-		VanIndexBar: typeof import("vant/es")["IndexBar"];
-		VanNavBar: typeof import("vant/es")["NavBar"];
-		VanPicker: typeof import("vant/es")["Picker"];
-		VanPopup: typeof import("vant/es")["Popup"];
-		VanSearch: typeof import("vant/es")["Search"];
-		VanTab: typeof import("vant/es")["Tab"];
-		VanTabs: typeof import("vant/es")["Tabs"];
-		VantLazyImg: typeof import("./../src/components/vant/VantLazyImg.vue")["default"];
-		VantList: typeof import("./../src/components/vant/VantList.vue")["default"];
-		VantListW: typeof import("./../src/components/vant/VantListW.vue")["default"];
-		VantNavBar: typeof import("./../src/components/vant/VantNavBar.vue")["default"];
-		VantPicker: typeof import("./../src/components/vant/VantPicker.vue")["default"];
-		VantTabs: typeof import("./../src/components/vant/VantTabs.vue")["default"];
-		VanUploader: typeof import("vant/es")["Uploader"];
-	}
+declare module '@vue/runtime-core' {
+  export interface GlobalComponents {
+    AreaCodePicker: typeof import('./../src/components/AreaCodePicker/AreaCodePicker.vue')['default']
+    Button: typeof import('./../src/components/Button/Button.vue')['default']
+    Carousel: typeof import('./../src/components/Carousel/Carousel.vue')['default']
+    Collapse: typeof import('./../src/components/Collapse/index.vue')['default']
+    CollapseItem: typeof import('./../src/components/Collapse/CollapseItem.vue')['default']
+    CollapseTransition: typeof import('./../src/components/Collapse/CollapseTransition.vue')['default']
+    copy: typeof import("./../src/components/SvgIcon/index copy.vue")["default"]
+    DateRangeSelect: typeof import('./../src/components/DateRangeSelect/DateRangeSelect.vue')['default']
+    FormInput: typeof import('./../src/components/Input/formInput.vue')['default']
+    Hcaptcha: typeof import('./../src/components/Hcaptcha/hcaptcha.vue')['default']
+    MultipleSelect: typeof import('./../src/components/MultipleSelect/MultipleSelect.vue')['default']
+    Nodata: typeof import('./../src/components/Nodata/index.vue')['default']
+    NoGameImg: typeof import('./../src/components/NoGameImg/NoGameImg.vue')['default']
+    Notify: typeof import('./../src/components/Notify/Notify.vue')['default']
+    RedbagRainCountdown: typeof import('./../src/components/redbagRainCountdown/index.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+    SingleSelect: typeof import('./../src/components/SingleSelect/SingleSelect.vue')['default']
+    Spin: typeof import('./../src/components/Spin/Spin.vue')['default']
+    SvgIcon: typeof import('./../src/components/svgIcon/index.vue')['default']
+    Tabs: typeof import('./../src/components/Tabs/Tabs.vue')['default']
+    VanActionSheet: typeof import('vant/es')['ActionSheet']
+    VanBadge: typeof import("vant/es")["Badge"]
+    VanButton: typeof import("vant/es")["Button"]
+    VanCell: typeof import("vant/es")["Cell"]
+    VanCellGroup: typeof import('vant/es')['CellGroup']
+    VanDatePicker: typeof import("vant/es")["DatePicker"]
+    VanDropdownItem: typeof import("vant/es")["DropdownItem"]
+    VanDropdownMenu: typeof import("vant/es")["DropdownMenu"]
+    VanField: typeof import('vant/es')['Field']
+    VanImage: typeof import("vant/es")["Image"]
+    VanIndexAnchor: typeof import('vant/es')['IndexAnchor']
+    VanIndexBar: typeof import('vant/es')['IndexBar']
+    VanNavBar: typeof import('vant/es')['NavBar']
+    VanPicker: typeof import('vant/es')['Picker']
+    VanPopup: typeof import('vant/es')['Popup']
+    VanSearch: typeof import("vant/es")["Search"]
+    VanTab: typeof import('vant/es')['Tab']
+    VanTabs: typeof import('vant/es')['Tabs']
+    VantLazyImg: typeof import('./../src/components/vant/VantLazyImg.vue')['default']
+    VantList: typeof import('./../src/components/vant/VantList.vue')['default']
+    VantListW: typeof import('./../src/components/vant/VantListW.vue')['default']
+    VantNavBar: typeof import('./../src/components/vant/VantNavBar.vue')['default']
+    VantPicker: typeof import('./../src/components/vant/VantPicker.vue')['default']
+    VantTabs: typeof import('./../src/components/vant/VantTabs.vue')['default']
+    VanUploader: typeof import('vant/es')['Uploader']
+  }
 }
