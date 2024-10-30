cd4a014a6a6703ff69448f9c8a8eb3475ff38a7a
suqin <suqin@whitesand.online>
2024-10-30 11:06:06 +0700

    fix

--- types/components.d.ts
+++ types/components.d.ts
@@ -34,7 +34,11 @@ declare module '@vue/runtime-core' {
     Tabs: typeof import('./../src/components/Tabs/Tabs.vue')['default']
     VanActionSheet: typeof import('vant/es')['ActionSheet']
     VanButton: typeof import('vant/es')['Button']
+    VanCellGroup: typeof import('vant/es')['CellGroup']
+    VanField: typeof import('vant/es')['Field']
     VanIcon: typeof import('vant/es')['Icon']
+    VanIndexAnchor: typeof import('vant/es')['IndexAnchor']
+    VanIndexBar: typeof import('vant/es')['IndexBar']
     VanList: typeof import('vant/es')['List']
     VanNavBar: typeof import('vant/es')['NavBar']
     VanNoticeBar: typeof import('vant/es')['NoticeBar']
@@ -49,5 +53,6 @@ declare module '@vue/runtime-core' {
     VantNavBar: typeof import('./../src/components/vant/VantNavBar.vue')['default']
     VantPicker: typeof import('./../src/components/vant/VantPicker.vue')['default']
     VantTabs: typeof import('./../src/components/vant/VantTabs.vue')['default']
+    VanUploader: typeof import('vant/es')['Uploader']
   }
 }
